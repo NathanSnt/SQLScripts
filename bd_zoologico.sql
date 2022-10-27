959f64ffdb7e97652a0a483e512529b20dc3d1cf
NATHAN DA SILVA SANTOS <natan-silva-2011@hotmail.com>
2022-10-27 16:33:00 -0300

    Zoologico

---
+++ bd_zoologico.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE zoologico;
+
+USE zoologico;
+CREATE TABLE animais(
+codigo INT,
+tipo CHAR(15),
+nome CHAR(30),
+idade TINYINT,
+valor DECIMAL(10,2)
+);
+
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (1, 'cachorro', 'Djudi', 3, 300.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (2, 'cachorro', 'Sula', 5, 300.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (3, 'cachorro', 'Sarina', 7, 300.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (4, 'gato', 'Pipa', 2, 500.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (5, 'gato', 'Sarangue', 2, 500.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (6, 'gato', 'Clarences', 1, 500.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (7, 'coruja', 'Agnes', 0, 700.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (8, 'coruja', 'Arabela', 1, 700.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (9, 'sapo', 'Quash', 1, 100.00);
+INSERT INTO animais(codigo, tipo, nome, idade, valor) VALUES (10, 'peixe', 'Fish', 0, 100.00);
+
+SELECT * FROM animais;
+
+SELECT tipo, nome FROM animais;
+SELECT tipo, nome, idade FROM animais;
+
+SELECT tipo AS "Tipo de Animal", nome AS "Nome do Animal" FROM animais;
+SELECT tipo AS "Tipo de Animal", nome AS "Nome do Animal", idade as "Tempo de Vida" FROM animais;
+
+ALTER TABLE animais ADD COLUMN (procedencia CHAR(17));
+
+UPDATE animais SET procedencia = 'Animal Doméstico';
+
+SELECT procedencia, tipo, nome, idade as 'Tempo de Vida' FROM  animais;
+
+ALTER TABLE animais ADD COLUMN (venda decimal(10, 2));
+
+UPDATE animais SET venda = 330.00 WHERE valor = 300.00;
+UPDATE animais SET venda = 550.00 WHERE valor = 500.00;
+UPDATE animais SET venda = 770.00 WHERE valor = 700.00;
+UPDATE animais SET venda = 110.00 WHERE valor = 100.00;
+
+SELECT tipo, nome, idade, valor AS 'Valor Original', venda AS 'Valor de Venda' FROM animais;
+
+SELECT tipo, valor AS 'Valor Original', venda AS 'Valor de Venda' FROM animais GROUP BY tipo;
