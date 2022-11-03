c6721c3cdc18ffc0e98097f74024c9aa790b6538
NATHAN DA SILVA SANTOS <natan-silva-2011@hotmail.com>
2022-11-03 17:18:21 -0300

    Aula Laércio 03-11-2022

--- dbloja.sql
+++ dbloja.sql
@@ -1,3 +1,12 @@
+/*
+----   COMANDOS MYSQL   ----
+show databases; -- Lista os bancos de dados existentes
+show tables; -- Lista todas as tabelas de um banco de dados
+use <nome do banco de dados>; -- seleciona um banco de dados
+desc <table>; -- descreve os campos de uma tabela
+source <endereço>; executa um escript
+*/
+
 -- Apagar banco de dados
 DROP DATABASE dbLoja;
 
@@ -23,9 +32,20 @@ PRIMARY KEY(codFunc)
 CREATE TABLE tbUsuarios(
 codUsu INT AUTO_INCREMENT,
 nome VARCHAR(15),
-senha VARCHAR(14),
+senha VARCHAR(20),
 PRIMARY KEY(codUsu)
 );
+
+CREATE TABLE tbProdutos(
+codProd INT AUTO_INCREMENT,
+descricao VARCHAR(50),
+preco DECIMAL(9, 2),
+validade DATE,
+quantidade INT,
+tipo VARCHAR(10),
+PRIMARY KEY(codProd)
+);
+
 
 -- Visualizar tabelas
 SHOW TABLES;
@@ -47,14 +67,48 @@ VALUES('maria.piedade', '12431243');
 INSERT INTO tbUsuarios(nome, senha)
 VALUES('jose.marahnao', '43214321');
 
+INSERT INTO tbProdutos(descricao, preco, validade, quantidade, tipo)
+VALUES('Banana', 20.50, '2022-11-07', 12, 'Dúzia');
+
+INSERT INTO tbProdutos(descricao, preco, validade, quantidade, tipo)
+VALUES('Banana', 10.25, '2022-11-07', 6, 'Meia-Dúzia');
+
+INSERT INTO tbProdutos(descricao, preco, validade, quantidade, tipo)
+VALUES('Banana', 15.50, '2022-11-07', 08, 'Unidade');
+
+INSERT INTO tbProdutos(descricao, preco, validade, quantidade, tipo)
+VALUES('Melância', 30.00, '2022-11-30', 1, 'Unidade');
+
+INSERT INTO tbProdutos(descricao, preco, validade, quantidade, tipo)
+VALUES('Morango', 10.50, '2022-11-08', 1, 'Caixa');
+
 -- Visualizando registros
 SELECT * FROM tbFuncionarios;
 SELECT * FROM tbUsuarios;
+SELECT * FROM tbProdutos;
+
+-- Busca por código
+SELECT * FROM tbProdutos WHERE codProd = 2;
+
+-- Busca por nome
+SELECT * FROM tbProdutos WHERE descricao LIKE 'melancia%';
+
+-- TIPO DE LIKE (%termo, termo% e %termo%)
+-- %nome Retorna tudo que termina com o termo nome
+-- nome% Retorna tudo que começa com o termo nome
+-- %nome% Retorna tudo que contém nome
 
 -- Alterando os registros
-UPDATE tbUsuarios SET senha = '12341243' WHERE codUsu = 1;
-SELECT * FROM tbUsuarios;
+UPDATE tbUsuarios SET senha = 'SenhaMuitoForte' WHERE codUsu = 1;
+-- SELECT * FROM tbUsuarios;
 
 -- Excluindo registros
-DELETE FROM tbUsuarios WHERE codUsu = 2;
-SELECT * FROM tbUsuarios;
+-- DELETE FROM tbUsuarios WHERE codUsu = 2;
+-- SELECT * FROM tbUsuarios;
+
+UPDATE tbprodutos SET preco = preco * 1.10 WHERE preco > 11;
+
+SELECT * FROM tbProdutos;
+
+-- Exibir sem repetir valores
+SELECT DISTINCT descricao FROM tbProdutos;
